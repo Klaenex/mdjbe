bda2e88b26a501d0f323b72d39241546c6391907
klaenex <hello@cuozzovincenzo.be>
2024-02-23 17:52:55 +0100

    route mdj

--- resources/js/Pages/Mdjs/index.jsx
+++ resources/js/Pages/Mdjs/index.jsx
@@ -4,19 +4,34 @@ import { Head, Link } from "@inertiajs/react";
 import Notification from "@/Components/Notification";
 
 export default function Dashboard({ auth }) {
+    const customizePaginationLabels = (link) => {
+        let label = link.label.replace("&laquo;", "«").replace("&raquo;", "»");
+
+        if (label.includes("Previous")) {
+            return "Précédent";
+        } else if (label.includes("Next")) {
+            return "Suivant";
+        }
+
+        return label;
+    };
     return (
         <AuthenticatedLayout
             user={auth.user}
             header={
                 <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    Dashboard
+                    Maisons de jeunes
                 </h2>
             }
         >
             <Head title="Maisons de jeunes" />
 
             <section className="my-10 mx-7 p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                test
+                <div className="flex justify-between items-center">
+                    <h1 className="text-white py-5 text-xl font-semibold ">
+                        Liste des maisons de jeunes
+                    </h1>
+                </div>
             </section>
         </AuthenticatedLayout>
     );
